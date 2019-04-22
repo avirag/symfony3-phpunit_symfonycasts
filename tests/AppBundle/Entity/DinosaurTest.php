9636dd6f09bc77ed6eba6b0689ec24b5695f98f1
Anita Virag <ik.virag@gmail.com>
2019-04-22 18:53:55 +0200

    Tests, Assertions & Coding

--- tests/AppBundle/Entity/DinosaurTest.php
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -2,13 +2,25 @@
 
 namespace Tests\AppBundle\Entity;
 
-use AppBundle\Entity\Dinosaur;
 use PHPUnit\Framework\TestCase;
+use AppBundle\Entity\Dinosaur;
 
 class DinosaurTest extends TestCase
 {
-    public function testThatYourComputerWorks()
+    public function testSettingLength()
     {
-        $this->assertTrue(true);
+        $dinosaur = new Dinosaur();
+        $this->assertSame(0, $dinosaur->getLength());
+
+        $dinosaur->setLength(9);
+        $this->assertSame(9, $dinosaur->getLength());
+    }
+
+    public function testDinosaurHasNotShrunk()
+    {
+        $dinosaur = new Dinosaur();
+        $dinosaur->setLength(15);
+
+        $this->assertGreaterThan(12, $dinosaur->getLength(), 'Did you put it in the washing machine?');
     }
 }
