c90b187a08bc230eb9be95548c50e700f0b80008
Anita Virag <ik.virag@gmail.com>
2019-04-23 22:35:18 +0200

    mocks & test doubles

--- src/AppBundle/Factory/DinosaurFactory.php
+++ src/AppBundle/Factory/DinosaurFactory.php
@@ -3,39 +3,29 @@
 namespace AppBundle\Factory;
 
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Service\DinosaurLengthDeterminator;
 
 class DinosaurFactory
 {
+    /**
+     * @var DinosaurLengthDeterminator
+     */
+    private $lengthDeterminator;
+
+    /**
+     * DinosaurFactory constructor.
+     * @param DinosaurLengthDeterminator $lengthDeterminator
+     */
+    public function __construct(DinosaurLengthDeterminator $lengthDeterminator)
+    {
+        $this->lengthDeterminator = $lengthDeterminator;
+    }
+
     private function createDinosaur(string $genus, bool $isCarnivorous, int $length): Dinosaur
     {
         $dinosaur = new Dinosaur($genus, $isCarnivorous);
         $dinosaur->setLength($length);
         return $dinosaur;
-    }
-    
-    private function getLengthFromSpecification(string $specification): int
-    {
-        $availableLengths = [
-            'huge' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            'omg' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            '😱' => ['min' => Dinosaur::HUGE, 'max' => 100],
-            'large' => ['min' => Dinosaur::LARGE, 'max' => Dinosaur::HUGE - 1],
-        ];
-        $minLength = 1;
-        $maxLength = Dinosaur::LARGE - 1;
-
-        foreach (explode(' ', $specification) as $keyword) {
-            $keyword = strtolower($keyword);
-
-            if (array_key_exists($keyword, $availableLengths)) {
-                $minLength = $availableLengths[$keyword]['min'];
-                $maxLength = $availableLengths[$keyword]['max'];
-
-                break;
-            }
-        }
-
-        return random_int($minLength, $maxLength);
     }
 
     public function growVelociraptor(int $length): Dinosaur
@@ -47,7 +37,7 @@ class DinosaurFactory
     {
         // defaults
         $codeName = 'InG-' . random_int(1, 99999);
-        $length = $this->getLengthFromSpecification($specification);
+        $length = $this->lengthDeterminator->getLengthFromSpecification($specification);
         $isCarnivorous = false;
 
         if(strpos($specification, 'carnivorous') !== false) {
